13d9be83d0ff3681fe8ca6267237a2f5129755ec
baongoc124@gmail.com <baongoc124@gmail.com@305612ba-3014-5c93-359c-37bf3b6318be>
2011-04-04 03:41:06 +0000

    Make db file reside in project folder. Don't need to change db setting everytime checkout anymore.

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for qlnt project.
+# Django settings for qlnt project
 
 # open path for template folder
 import os.path
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/rmrf/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), 'sqlite3.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
