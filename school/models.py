5f1bf50fc575ccf7e3774589ad86b313a5cc1d64
vu.tran54@gmail.com <vu.tran54@gmail.com@305612ba-3014-5c93-359c-37bf3b6318be>
2011-04-08 18:34:45 +0000

    fixed some syntax errors and changed the name attribute to first_name and last_name

--- school/models.py
+++ school/models.py
@@ -9,8 +9,9 @@ class School(models.Model):
 	web = models.URLField(null = True)
 
 class CommonInfo(models.Model):
-	name = models.CharField(max_length = 45)
-	birthday = models.DateField()
+	first_name = models.CharField(max_length = 45)
+	last_name = models.CharField(max_length = 45) # tach ra first_name and last_name de sort va import from excel file
+   birthday = models.DateField()
 	birth_place = models.CharField(max_length = 200)
 	sex = models.CharField(max_length = 2, choices = GENDER_CHOICES)
 	phone = models.CharField(max_length = 15, null = True)
@@ -19,7 +20,7 @@ class CommonInfo(models.Model):
 	class Meta:
 		abstract = True
 		
-class Teacher(CommonInfo):
+class Teacher(CommonInfo): pass
 	
 class Class(models.Model):
 	class_code = models.CharField(max_length = 20, unique = True)
@@ -36,10 +37,10 @@ class Pupil(CommonInfo):
 	father_name = models.CharField(max_length = 45)
 	father_birthday = models.DateField()
 	father_phone = models.CharField(max_length = 15, null = True)
-	father_job = models.CharField(max_leng = 100)
+	father_job = models.CharField(max_length = 100)
 	mother_name = models.CharField(max_length = 45)
 	mother_birthday = models.DateField()
 	mother_phone = models.CharField(max_length = 15, null = True)
-	mother_job = models.CharField(max_leng = 100)
+	mother_job = models.CharField(max_length = 100)
 	school_id = models.ForeignKey(School)
 	class_id = models.ForeignKey(Class)
