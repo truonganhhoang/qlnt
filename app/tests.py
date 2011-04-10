6e61e6a2265c2e70cea3eb89fddec09a6383d808
quyendt2612@gmail.com <quyendt2612@gmail.com@305612ba-3014-5c93-359c-37bf3b6318be>
2011-04-10 16:52:45 +0000

    add unittest for login, logout, create user, group. Still need more help on logout and delete function

--- app/tests.py
+++ app/tests.py
@@ -5,12 +5,65 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class UserFunctionTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_login(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        
+    
+    def test_logout(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        '''
+        NEED MORE HELP!
+        '''
+        #response = self.client.post('/logout/')
+        #self.assertEqual(response.status_code, 301)
+        
+    def test_user_creation(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        data = {
+            'username': 'st01',
+            'password1': 'st01',
+            'password2': 'st01'
+        }
+        response = self.client.post('/admin/auth/user/add/', data)
+        self.assertEqual(response.status_code, 302)
+    
+    def test_user_deletion(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        data = {
+            'username': 'st02',
+            'password1': 'st02',
+            'password2': 'st02'
+        }
+        response = self.client.post('/admin/auth/user/add/', data)
+        self.assertEqual(response.status_code, 302)
+        '''NEED HELP!
+        which data will be sent when deleting action executed
+        '''
+        #data = {
+        #    'action': 'delete_selected',
+        #    '_selected_action': '2'
+        #}
+        #response = self.client.post('/admin/auth/user/', )
+        #self.assertEqual(response.status_code, 302)
+    
+    def test_group_creation(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 302)
+        data = {
+            'name': 'teachers'
+        }
+        response = self.client.post('/admin/auth/group/add/', data)
+        self.assertEqual(response.status_code, 302)
+        
