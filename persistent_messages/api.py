2338ad8b4bb0e774380aa1303f1d0f142294d265
truonganhhoang <truonganhhoang@305612ba-3014-5c93-359c-37bf3b6318be>
2011-04-29 03:02:35 +0000

    fix compile errors

--- persistent_messages/api.py
+++ persistent_messages/api.py
@@ -8,20 +8,20 @@ def add_message(request, level, message, extra_tags='', fail_silently=False, sub
         notify.email(level, message, extra_tags, subject, user, from_user)
     return request._messages.add(level, message, extra_tags, subject, user, from_user, expires, close_timeout)
 
-def info(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
-    """
-    """
-    level = constants.INFO
-    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
-
-def warning(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
-    """
-    """
-    level = constants.WARNING
-    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
-
-def debug(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
-    """
-    """
-    level = constants.DEBUG
-    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
+#def info(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+#    """
+#    """
+#    level = constants.INFO
+#    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
+#
+#def warning(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+#    """
+#    """
+#    level = constants.WARNING
+#    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
+#
+#def debug(request, message, extra_tags='', fail_silently=False, subject='', user=None, email=False, from_user=None, expires=None, close_timeout=None):
+#    """
+#    """
+#    level = constants.DEBUG
+#    return add_message(request, level, message, extra_tags, fail_silently, subject, user, email, from_user, expires, close_timeout)
