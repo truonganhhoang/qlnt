065bfb6f00f0f47ca6f5f11f18d67df4c483ead4
quyendt2612 <quyendt2612@305612ba-3014-5c93-359c-37bf3b6318be>
2011-05-06 03:59:15 +0000

    Complete manual sms. With no user for testing, i just guess it works properly. Uncomment to send with viettel sms system

--- sms/views.py
+++ sms/views.py
@@ -3,6 +3,8 @@ from sms.models import sms, smsForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
+from django.test.client import Client
+import urllib, urllib2
 
 def index(request):
     return render_to_response("sms/index.html", context_instance=RequestContext(request))
@@ -15,15 +17,34 @@ def manual_sms(request):
             content = form.cleaned_data.get('content')
             
             phone = phone_list.split(',')
-            for p in phone:            
+            open = urllib2.build_opener( urllib2.HTTPCookieProcessor() )
+            urllib2.install_opener( open )
+            
+            para = urllib.urlencode({'u': 'VT_username', 'p': 'VT_password'})
+            
+#            f = open.open('http://viettelvas.vn:7777/fromcp.asmx', para)
+#            f.close();
+            
+            for p in phone:    
                 '''Save to db'''
                 s = sms(phone=p, content=content)
                 s.save()
+                
                 '''Send sms via Viettel system'''
                 '''Implement Me'''
+                data = urllib.urlencode({
+                            'RequestID'     : '4',
+                            'CPCode'        : '',
+                            'UserID'        : '',
+                            'ReceiverID'    : p,
+                            'ServiceID'     : '',
+                            'CommandCode'   : '',
+                            'Content'       : content,
+                            'ContentType'   : '',
+                            })
+#                f = open.open('http://viettelvas.vn:7777/fromcp.asmx', data)
             
             return HttpResponseRedirect('/sms/sent_sms/')
-#            return HttpResponse(phone_list)
     else:
         form = smsForm()    
     t = loader.get_template('sms/manual_sms.html')
