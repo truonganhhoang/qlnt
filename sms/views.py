f5a511b6f9d50e3eccde73e5c29ce2923bfebcc6
quyendt2612 <quyendt2612@305612ba-3014-5c93-359c-37bf3b6318be>
2011-05-14 14:48:33 +0000

    add function sendSMS() for send a single sms

--- sms/views.py
+++ sms/views.py
@@ -14,7 +14,37 @@ import xlwt
 
 TEMP_FILE_LOCATION = os.path.join(os.path.dirname(__file__), 'uploaded')
 EXPORTED_FILE_LOCATION = os.path.join(os.path.dirname(__file__), 'exported')
-        
+
+def sendSMS(phone,content,user):
+    open = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    urllib2.install_opener(open)
+            
+    para = urllib.urlencode({'u': 'VT_username', 'p': 'VT_password'})
+            
+    f = open.open('http://viettelvas.vn:7777/fromcp.asmx', para)
+    f.close();
+    
+    if checkValidPhoneNumber(phone):    
+        '''Save to db'''
+        s = sms(phone=phone, content=content, sender=user, recent=True, success=True)
+        s.save()
+                    
+        '''Send sms via Viettel system'''
+        data = urllib.urlencode({
+                        'RequestID'     : '4',
+                        'CPCode'        : '',
+                        'UserID'        : '',
+                        'ReceiverID'    : phone,
+                        'ServiceID'     : '',
+                        'CommandCode'   : '',
+                        'Content'       : content,
+                        'ContentType'   : ''})
+#        f = open.open('http://viettelvas.vn:7777/fromcp.asmx', data)
+    else:    
+        '''Save to db'''
+        s = sms(phone=phone, content=content, sender=user, recent=True, success=False)
+        s.save()
+
 def checkValidPhoneNumber(phone):
     user_list = User.objects.all()
     user_phone_list = []
