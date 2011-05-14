e4310c244cb4ede8d9f7dd4bec73bc06160f3219
baongoc124@gmail.com <baongoc124@gmail.com@305612ba-3014-5c93-359c-37bf3b6318be>
2011-05-14 07:03:59 +0000

    Resolve minor sms problem

--- sms/urls.py
+++ sms/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^manual_sms/$', 'sms.views.manual_sms'),
     url(r'^excel_sms/$', 'sms.views.excel_sms'),
     url(r'^sent_sms/$', ListView.as_view(
-            queryset=sms.objects.filter(created__range=(datetime.now() - timedelta(hours=0, minutes=1, seconds=0),datetime.now())),
+            get_queryset=lambda :sms.objects.filter(created__range=(datetime.now() - timedelta(hours=0, minutes=1, seconds=0),datetime.now())),
             context_object_name='sms_list',
             template_name='sms/sent_sms.html')),
     url(r'^export_excel/$', 'sms.views.export_excel')
