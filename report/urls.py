d117cda700afed3b08389c596867aa0bb7803c7c
ngocthanh2207@gmail.com <ngocthanh2207@gmail.com@305612ba-3014-5c93-359c-37bf3b6318be>
2011-05-15 08:59:57 +0000

    chinh sua giao dien cap tren truong

--- report/urls.py
+++ report/urls.py
@@ -1,11 +1,6 @@
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from app.models import User
-
 from django.conf.urls.defaults import patterns, include, url
-import settings 
 
 urlpatterns = patterns('',
     # Add, remove, change personal information
-    
+    url(r'^$', 'report.views.base_report'),    
 )
--- report/views.py
+++ report/views.py
@@ -1,1 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from app.models import UserForm, Organization, UserProfile
+#from app.models import PositionTypeForm
+from django.template import RequestContext, loader
+from django import forms
+from django.shortcuts import render_to_response
 # Create your views here.
+def base_report(request): 
+    return render_to_response("base_report.html", context_instance=RequestContext(request))
