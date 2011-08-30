478bbe569b1ba91a7b8767e782b42550f0c428e2
luulethe <luulethe@305612ba-3014-5c93-359c-37bf3b6318be>
2011-08-30 17:55:25 +0000

    make code shorter

---
+++ school/templatetags/mark.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def convertMarkToCharacter(x):
+    if x==1:
+        return "aaaa"
+    else:
+        return """ bbbb"""
+
+@register.filter
+def convertHlToVietnamese(x):
+    if x=='G':
+        return u'Giỏi'
+    elif x=='K':
+        return u'Khá'
+    elif x=='TB':
+        return u'TB'
+    elif x=='Y':
+        return u'Yếu'
+    elif x=='Kem':
+        return u'Kém'
+    else:
+        return u''    
+@register.filter
+def convertMarkToCharacter(x):
+    if x==9:
+        return u'Giỏi'
+    elif x==7:
+        return u'Khá'
+    elif x==6:
+        return u'TB'
+    elif x==4:
+        return u'Yếu'
+    elif x==1:
+        return u'Kém'
+    else:
+        return  u''   
