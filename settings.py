17d6713de45c4a5ae69e0fd478b6e9ab61f00760
truonganhhoang <truonganhhoang@305612ba-3014-5c93-359c-37bf3b6318be>
2011-08-31 10:20:48 +0000

    add html and css, js compression. Need pip install django_compressor and django-slimmer

--- settings.py
+++ settings.py
@@ -84,23 +84,23 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 #MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/'))
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'static')
+#MEDIA_ROOT = os.path.join(SITE_ROOT, 'static')
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/static/'
+#MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = ''
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -139,6 +139,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'pagination.middleware.PaginationMiddleware',
+    #'htmlmin.middleware.HtmlMinifyMiddleware',
+    'slimmer.middleware.CompressHtmlMiddleware',
+
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -171,7 +174,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -192,10 +195,17 @@ INSTALLED_APPS = (
     'sentry.client', #and viewing exceptions
 
     'south', #for database migration/upgrade
-
+    'compressor', #for css and js compression
 #    'nose', #nose must come after south.
     
 #    'test_utils',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -252,4 +262,3 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST_USER = ''
 
 EMAIL_SUBJECT_PREFIX = '[QLNT]'
-
